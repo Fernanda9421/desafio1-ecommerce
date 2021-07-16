11ea7f76591d65fa03eef4faf257cd765e8460ca
Fernanda9421 <fer.candrade21@gmail.com>
2021-07-16 07:47:38 -0300

    Ajuste do formulário de email - localStorage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var entrada = document.getElementById('email');
-
+localStorage.setItem('emails',JSON.stringify(['']));
 
 document.getElementById('botao2').addEventListener('click', function() {
-    localStorage.email = entrada.value;
+    const emails = JSON.parse(localStorage.getItem('emails'));
+    console.log('emails', emails)
+    emails.push(entrada.value);
+    localStorage.setItem('emails',JSON.stringify(emails));
 });
